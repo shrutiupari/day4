f63f451a944bced9db5d183ade6664133962e9f8
shrutiupari <shrutiupari@users.noreply.github.com>
2017-05-12 06:31:56 +0530

    Add files via upload

---
+++ content.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+.style45 {font-size: 16px; color: #0000FF; font-family: Arial, Helvetica, sans-serif; }
+.style46 {font-size: 18px; color: #0000FF;}
+-->
+</style>
+</head>
+
+<body>
+<table width="365" border="1" cellspacing="20" cellpadding="20">
+  <tr>
+    <th width="281" scope="row"><p align="left">Your Name: </p>
+    <p>&nbsp;</p></th>
+  </tr>
+  <tr>
+    <th scope="row"><div align="left">Email ID: </div></th>
+  </tr>
+  <tr>
+    <th scope="row"><div align="left">Gender</div></th>
+  </tr>
+</table>
+<p>&nbsp;</p>
+</body>
+</html>
